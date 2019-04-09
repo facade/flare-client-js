836be4dcb65200005fe971b4d4dc1e8b5c3f1054
Adriaan <adriaanmarain300@gmail.com>
2019-04-09 17:44:14 +0200

    do not report useless errors without a message (closes #21)

--- packages/client/reporter.ts
+++ packages/client/reporter.ts
@@ -12,11 +12,20 @@ export interface Context {
 }
 
 export function reportError(error: Error, context: Context = {}) {
+    if (!error.message) {
+        console.error(
+            `Flare JS Client: the error object that was passed to
+            flareClient.reportError() does not have a message key.
+            This error will not be reported to the server.`
+        );
+        return;
+    }
+
     errorToFormattedStacktrace(error).then(stacktrace => {
         const body = {
             key: flareClient.key,
             notifier: 'Flare JavaScript Client V1.0', // TODO: get version dynamically from package.json (webpack env plugin?),
-            exceptionClass: error.constructor.name,
+            exceptionClass: error.constructor ? error.constructor.name : undefined,
             seenAt: getCurrentTime(),
             message: error.message,
             language: 'javascript',
