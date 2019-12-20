cc6090524c88e108cdc995373db0d938d6e61187
Adriaan <adriaanmarain300@gmail.com>
2019-12-20 14:34:55 +0100

    handle rejected promises

--- packages/client/src/browserClient/index.ts
+++ packages/client/src/browserClient/index.ts
@@ -6,6 +6,7 @@ export default function catchWindowErrors() {
     }
 
     const originalOnerrorHandler = window.onerror;
+    const originalOnunhandledrejectionHandler = window.onunhandledrejection;
 
     window.onerror = (_1, _2, _3, _4, error) => {
         if (error) {
@@ -14,6 +15,18 @@ export default function catchWindowErrors() {
 
         if (typeof originalOnerrorHandler === 'function') {
             originalOnerrorHandler(_1, _2, _3, _4, error);
+        }
+    };
+
+    window.onunhandledrejection = (error: PromiseRejectionEvent) => {
+        if (error.reason instanceof Error) {
+            flare.report(error.reason);
+        }
+
+        // TODO: Maybe also send errors without stacktrace for unhandled rejections without an Error as reason? (could be a string, …)
+
+        if (typeof originalOnunhandledrejectionHandler === 'function') {
+            originalOnunhandledrejectionHandler(error);
         }
     };
 }
