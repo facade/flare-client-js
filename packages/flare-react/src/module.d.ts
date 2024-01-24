dced7b767e62ace3b53b5c42577f7781799bbc78
sebastiandedeyne <sebastiandedeyne@gmail.com>
2024-01-24 16:17:09 +0100

    Remove unused type file

--- packages/flare-react/src/module.d.ts
+++
@@ -1,9 +0,0 @@
-type Flare = import('@flareapp/flare-client/dist/FlareClient').default;
-
-interface Window {
-    flare: Flare;
-}
-
-declare module '@flareapp/flare-client' {
-    export const FlareClient: string;
-}
